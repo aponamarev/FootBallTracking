Fixed: forgot to square the loss to elliminate the dependency on value sign

src/NetTemplate.py
@@ -69,7 +69,7 @@ class NetTemplate(object):
 
             self._collect_parameter_stats(W)
 
-            conv = tf.nn.conv2d(inputs, W, strides, padding)
+            conv = tf.nn.conv2d(inputs, W, strides, padding, name="conv")
 
             if bias:
                 b_size = shapes[3]
@@ -78,15 +78,19 @@ class NetTemplate(object):
                 conv = tf.nn.bias_add(conv, b, data_format='NHWC')
                 self._collect_parameter_stats(b)
 
-        return self._activation(conv)
+                conv = self._activation(conv)
+
+        return conv
 
 
     def _deconv(self, input, filters, kernel_size, strides=[2,2], padding="SAME", name="deconv", bias=True):
 
-        conv = tf.layers.conv2d_transpose(input, filters, kernel_size, strides, padding,
-                                          use_bias=bias, kernel_initializer=xavier_initializer(), name=name)
+        with tf.variable_scope(name):
+            conv = tf.layers.conv2d_transpose(input, filters, kernel_size, strides, padding,
+                                              use_bias=bias, kernel_initializer=xavier_initializer(), name="deconv")
+            conv = self._activation(conv)
 
-        return self._activation(conv)
+        return conv
 
 
     def _concat(self, *arg, axis=3, name='merge'):

src/ObjectDetection.py
@@ -320,19 +320,23 @@ class ObjectDetectionNet(NetTemplate):
 
                     anchor_mask = reshape(mask, [self.batch_sz, WHK])
 
-                    self.conf_loss = reduce_mean(
-                        reduce_sum(
-                            W_pos / n_obj * tf.square(self.IoU - self.anchor_confidence) * anchor_mask
-                            + (1 - anchor_mask) * self.anchor_confidence * W_neg / (WHK - n_obj),
-                            1), name='confidence_loss')
+                    conf_pos = truediv(reduce_sum(tf.square(self.IoU - self.anchor_confidence) * anchor_mask,1,
+                                                  keep_dims=True), n_obj)
+                    conf_neg = truediv(reduce_sum(tf.square((1 - anchor_mask) * self.anchor_confidence),1,
+                                                  keep_dims=True), WHK - n_obj)
+
+                    self.conf_loss = reduce_mean(W_pos * conf_pos + W_neg * conf_neg, name='confidence_loss')
 
                     tf.add_to_collection(tf.GraphKeys.LOSSES, self.conf_loss)
 
                 with tf.variable_scope('BBox'):
-                    self.bbox_loss = reduce_mean(truediv(
-                        reduce_sum(
-                            W_bbox * tf.square(mask * (self.detected_box_deltas - self.input_box_delta)), 1
-                        ), n_obj), name='bbox_loss')
+
+                    all_deltas = self.detected_box_deltas - self.input_box_delta
+                    pos_deltas = all_deltas * mask
+                    norm_deltas = reduce_sum(tf.square(pos_deltas), 1)
+                    norm_deltas = reduce_sum(truediv(norm_deltas, n_obj),1)
+
+                    self.bbox_loss = W_bbox * reduce_mean(norm_deltas, name='bbox_loss')
 
                     tf.add_to_collection(tf.GraphKeys.LOSSES, self.bbox_loss)
 

train.py
@@ -147,8 +147,8 @@ def train():
             _, loss_value, conf_loss, bbox_loss, class_loss = \
                 sess.run([net.train_op, net.loss, net.conf_loss, net.bbox_loss, net.P_loss],
                          feed_dict={net.is_training_mode: True}) # , feed_dict={net.dropout_keep_rate: 0.75}
-            pbar.set_postfix(total_loss="{:.1f}".format(loss_value),
-                             bbox_loss="{:.2f}".format(bbox_loss), class_loss="{:.2f}".format(class_loss))
+            pbar.set_postfix(conf_loss="{:.2f}".format(conf_loss),
+                             total_loss="{:.2f}".format(loss_value))
 
         assert not np.isnan(loss_value), \
             'Model diverged. Total loss: {}, conf_loss: {}, bbox_loss: {}, ' \