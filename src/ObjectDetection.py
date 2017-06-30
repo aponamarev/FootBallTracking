Fixed queuing pipeline. Fixed image normalization. Added summaries for n ofbjects

src/ObjectDetection.py
@@ -309,7 +309,10 @@ class ObjectDetectionNet(NetTemplate):
         W_pos = self.LOSS_COEF_CONF_POS # weight of confidence loss in positive examples
         W_neg = self.LOSS_COEF_CONF_NEG # weight of confidence loss in negative examples
         n_obj = reduce_sum(mask) # number of target objects in an image - used to normalize loss
-        tf.summary.scalar("num_objects", reduce_mean(reduce_sum(mask,1)))
+        n_obj_per_batch = reduce_sum(mask, 1)
+        tf.summary.scalar("n_objects/avg", reduce_mean(n_obj_per_batch))
+        tf.summary.scalar("n_objects/min", tf.reduce_min(n_obj_per_batch))
+        tf.summary.scalar("n_objects/max", tf.reduce_max(n_obj_per_batch))
         WHK = self.WHK # feature map width * height * K anchors per cell
         L = self.input_labels
         ɛ = self.EPSILON

train.py
@@ -125,14 +125,15 @@ def train():
     config.allow_soft_placement = True
     sess = tf.Session(config=config, graph=graph)
     sess.run(initializer)
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
     if restore_model:
         saver = tf.train.Saver(graph.get_collection(tf.GraphKeys.GLOBAL_VARIABLES))
         saver.restore(sess, join(train_dir, 'model.ckpt'))
 
     tf.train.start_queue_runners(sess=sess, coord=coord)
-    threads = [threading.Thread(target=enqueue_thread, args=(coord, sess, net, enqueue_op, inputs)).start()
-               for _ in range(prefetching_threads)]
+    [threading.Thread(target=enqueue_thread, args=(coord, sess, net, enqueue_op, inputs)).start()
+     for _ in range(prefetching_threads)]
 
     pass_tracker_start = time.time()
     pass_tracker_prior = pass_tracker_start